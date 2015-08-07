bfe20b08e93fd60d96462dd70b0a6b6aa2471a24
Siddhartha Basu <biosidd@gmail.com>
2015-08-07 21:51:54 +0000

    Added debugtool

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,25 @@ import React from 'react';
 import {Provider} from 'react-redux';
 import App from './containers/App';
 import * as store from './stores';
+import { DevTools, LogMonitor, DebugPanel } from 'redux-devtools/lib/react';
+
+function renderDevTools() {
+    if (__DEBUG__) {
+      return (
+        <DebugPanel top left bottom key='debugPanel'>
+          <DevTools store={store} monitor={LogMonitor} />
+        </DebugPanel>
+      );
+    }
+}
 
 const elem = document.getElementById('container');
 React.render(
-    <Provider store={store}>
-        {() => <App/>}
-    </Provider>,
+    <div>
+        <Provider store={store}>
+            {() => <App/>}
+        </Provider>
+        {renderDevTools()}
+    </div>,
     elem
 );
