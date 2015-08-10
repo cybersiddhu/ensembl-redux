3b639015a03bdd6b00243ccc88878d8291bffeb7
Siddhartha Basu <biosidd@gmail.com>
2015-08-10 21:04:31 +0000

    Added action creator for fetching overlapping features

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,13 @@
-import * as types from '../constants';
+import types from '../constants';
+import axios from 'axios';
+
+const overlapURL = 'http://rest.ensembl.org/overlap/id/';
+
+export function getOverlap(id) {
+    const url = [overlapURL, id, '?feature=gene'].join('');
+    return {
+        types: types,
+        promise: axios.get(url, {headers: {'Content-type': 'application/json'}}),
+        id
+    };
+}
