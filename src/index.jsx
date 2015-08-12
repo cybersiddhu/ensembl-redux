5b2fd225b9dce2f95e1ea41f0525cbf7a6e461e6
Siddhartha Basu <biosidd@gmail.com>
2015-08-12 08:57:34 +0000

    Passed an instance of HashHistory instead of direct import

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,9 @@
+import 'babel-core/polyfill';
 import React from 'react';
-import {Provider} from 'react-redux';
-import {App} from './containers/App';
-import * as store from './stores';
-import { DevTools, LogMonitor, DebugPanel } from 'redux-devtools/lib/react';
+import Root from './containers/Root';
+import HashHistory from 'react-router/lib/HashHistory';
 
-function renderDevTools() {
-    if (__DEBUG__) {
-      return (
-        <DebugPanel top left bottom key='debugPanel'>
-          <DevTools store={store} monitor={LogMonitor} />
-        </DebugPanel>
-      );
-    }
-}
-
-const elem = document.getElementById('container');
 React.render(
-    <div>
-        <Provider store={store}>
-            {() => <App/>}
-        </Provider>
-        {renderDevTools()}
-    </div>,
-    elem
+    <Root history={new HashHistory()}/>,
+        document.getElementById('container')
 );
