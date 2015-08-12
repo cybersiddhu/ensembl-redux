6ef8a6b7d7a77961b58c7b19eff9447f497cf827
Siddhartha Basu <biosidd@gmail.com>
2015-08-12 08:59:26 +0000

    Added conditional deactivation of eslint checking

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,14 +1,16 @@
-import { applyMiddleware, compose, createStore, combineReducers } from 'redux';
-import { devTools } from 'redux-devtools';
+import {applyMiddleware, compose, createStore, combineReducers} from 'redux';
+import {devTools} from 'redux-devtools';
 import * as reducers from 'reducers';
 import {ensemblMiddleware} from '../middleware';
 
-var buildStore;
+let buildStore;
 
+/* eslint-disable */
 if (__DEBUG__) {
-  buildStore = compose(applyMiddleware(ensemblMiddleware), devTools(), createStore);
+/* eslint-enable */
+    buildStore = compose(applyMiddleware(ensemblMiddleware), devTools(), createStore);
 } else {
-  buildStore = compose(applyMiddleware(ensemblMiddleware), createStore);
+    buildStore = compose(applyMiddleware(ensemblMiddleware), createStore);
 }
 
 export default buildStore(combineReducers(reducers));
