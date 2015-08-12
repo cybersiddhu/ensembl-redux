ef91af913fe59dc0c0ba8061e963069449e64e80
Siddhartha Basu <biosidd@gmail.com>
2015-08-12 08:56:36 +0000

    Removed the explicit rendering of child component Fixed bunch of syntax errors reported by eslint Changed one of the field name of component state

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,23 +4,34 @@ import * as actionCreators from '../actions';
 import {bindActionCreators} from 'redux';
 
 class App extends Component {
+    displayName = 'default app'
     render() {
-
+        const {children} = this.props;
+        return (
+            <div>
+                <h2> Ensembl Id information displayer </h2>
+                <h3> Load /ensembl/ensembl_id in your browser</h3>
+                {children}
+            </div>
+        );
     }
 }
 
 // Which part of the Redux global state does our component want to receive as props
 function mapStateToProps(state) {
     return {
-
-    }
+        id: state.id,
+        loading: state.loading,
+        response: state.response,
+        error: state.error
+    };
 }
 
 // Which action creators does it want to receive by props
 function mapDispatchToProps(dispatch) {
     return {
         actions: bindActionCreators(actionCreators, dispatch)
-    }
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
